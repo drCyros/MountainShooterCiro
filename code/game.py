1f62b9237ef9326c55988359dd608ee51734fcbe
Ciro <cirorenatolima@gmail.com>
2024-10-01 21:21:08 -0400

    Moving starter from main.py to game.py

--- code/game.py
+++ code/game.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
 
 class Game:
     def __init__(self):
         self.window = None
 
     def run(self, ):
-        pass
+        print("Setup Start")
+        pygame.init()
+        window = pygame.display.set_mode((600, 480))
+        print("Setup Done")
+
+        print("Loop Start")
+        while True:
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:  # Se o evento for do tipo pygame.QUIT
+                    print("Quitting...")
+                    pygame.quit()  # Close window
+                    quit()  # End pygame
--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame
+from code.game import Game
 
-print("Setup Start")
-pygame.init()
-window = pygame.display.set_mode((600, 480))
-print("Setup Done")
-
-print("Loop Start")
-while True:
-    # Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # Se o evento for do tipo pygame.QUIT
-            pygame.quit()  # Close window
-            quit()  # End pygame
+game = Game()
+game.run()
